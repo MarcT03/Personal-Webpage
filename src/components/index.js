9ee2d53126896b9016aefd3a85b2c9f707952c6b
mat319 <831cheesehead@gmail.com>
2024-12-29 20:05:49 -0800

    Exported all components using index.js.

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+
+// src/App.jsx
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Navbar } from './Navbar';
+export { default as Projects } from './Projects';
+export { default as Tech } from './Tech';
+
