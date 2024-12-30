a6a28fec3b8d2d461de4550dfbcc0863cbe18bf6
mat319 <831cheesehead@gmail.com>
2024-12-29 20:01:49 -0800

    Added new backgrownd colors and fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'light-bg': '#E0E0E0',
+        'dark-bg': '#121212',
+        'light-text': '#121212',
+        'dark-text': '#E0E0E0'
+      },
+      fontFamily: {
+        helvetica: ['"Helvetica Neue"', 'Arial', 'sans-serif'],
+        sf: ['"SF Pro"', '-apple-system', 'BlinkMacSystemFont', '"Helvetica Neue"', 'Arial', 'sans-serif'], // Example for testing
+      },
+    },
   },
   plugins: [],
 }
