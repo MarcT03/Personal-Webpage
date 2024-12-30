a86b0ea450f550cb7e69548b5bc35eeaccd11697
mat319 <831cheesehead@gmail.com>
2024-12-29 20:03:32 -0800

    Added Navbar and light mode and dark mode colors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Navbar, Projects, Tech } from './components';
 const App = () => {
   return(
-    <div>
-      Marc's Dev Portfolio
+    <BrowserRouter>
+    <div className=" relative z-0 bg-light-bg dark:bg-dark-bg">
+        <div className="bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          
+        </div>
     </div>
+    </BrowserRouter>
+
   )
     
 }
